0be86d6f76209bbe50c91a7b874b93ac30e57eff
jose <josediazisnow@gmail.com>
2019-08-06 14:29:44 -0500

    Initial commit

---
+++ SplashPage/loginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  loginViewController.swift
+//  SplashPage
+//
+//  Created by SER-YCL7 on 7/15/19.
+//  Copyright © 2019 CSApps. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController:UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+}
---
+++ SplashPage/viewcontrollerlogin.swift
@@ -0,0 +1,12 @@
+//
+//  viewcontrollerlogin.swift
+//  SplashPage
+//
+//  Created by SER-YCL7 on 7/23/19.
+//  Copyright © 2019 CSApps. All rights reserved.
+//
+
+import UIKit
+//in case that we need to customize the login page
+class viewcontrollerlogin: UIViewController {
+}
